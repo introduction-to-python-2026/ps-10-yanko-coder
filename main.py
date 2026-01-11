a55dbe717db8fc6d49ed096d8719a3f61282dcac
yanko-coder <yannaykohn@gmail.com>
2026-01-11 19:12:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-
+seal_image = load_image('/content/Screenshot 2025-05-13 073842.png')
+clean_seal_image = median(seal_image, ball(3))
+edge_image = edge_detection(clean_seal_image)
+binary = (edge_image > 80).astype(np.uint8)
